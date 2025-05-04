365e9d04ff7af61238c611419e81551940a535a2
30Piraten <victorehikioyaraeva@outlook.com>
2025-05-05 00:00:14 +0100

    Updated hero and added twitter icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import {loadavg} from "node:os";
 
 // Create __dirname equivalent in ES modules
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -70,7 +71,7 @@ function getTitleFromFile(filepath: string): string {
 export default defineConfig({
 
   lang: "en-US",
-  title: "rayvah.cc",
+  title: "rev",
   description: "systems-minded product and infrastructure engineer",
 
   lastUpdated: true,
@@ -80,8 +81,6 @@ export default defineConfig({
 
   // ✅ Favicon + Fonts must go here, not in themeConfig - I forgot the last time
   head: [
-    // ["link", { rel: "icon", href: "/favicon.ico" }],
-    // ["link", { rel: "stylesheet", href: "./theme/custom.css" }],
     ["link", { rel:"icon", type:"image/svg+xml", href: "/favicon.svg?v=1" }],
     ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
     ["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" }],
@@ -108,28 +107,12 @@ export default defineConfig({
 
     // Add assets handling plugin
     plugins: [
-      // {
-      //   name: 'vitepress-images-resolver',
-      //   enforce: 'pre',
-      //   resolveId(id) {
-      //     if (id.startsWith('/images/')) {
-      //       // Strip leading slash to get correct relative path
-      //       // const imagePath = id.replace(/^\/+/, '');
-      //       // return resolve(__dirname, '..', imagePath);
-      //       return {
-      //         id: resolve(__dirname, "../public", id),
-      //         external: true
-      //       };
-      //     }
-      //     return null;
-      //   }
-      // }
     ]
   },
   
   themeConfig: {
     logo: "/logo.png",
-    siteTitle: "rayvah",
+    siteTitle: "Victor Raeva",
 
     notFound: {
       code: '404',
@@ -150,12 +133,21 @@ export default defineConfig({
     socialLinks: [
       { icon: "github", link: "https://github.com/30Piraten/" },
       { icon: "linkedin", link: "https://www.linkedin.com/in/evictor3/" },
+      { icon: "twitter", link: "https://x.com/raeva33" }
+      // {
+      //   icon: {
+      //     svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      //         <path fill="currentColor" d="m20 8l-8 5l-8-5V6l8 5l8-5m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Z" />
+      //       </svg>`
+      //   },
+      //   link: "mailto:raeva303@gmail.com"
+      // }
     ],
 
     // ✅ Footer config
     footer: {
       message: "mit liebe gebaut",
-      copyright: "Copyright ©️ 2025–present | raeva",
+      copyright: "Copyright ©️ 2025–present | Victor Raeva",
     },
 
     sidebar: {
