97a958b13384dca9920d8956ad2c06787ee791d2
30Piraten <victorehikioyaraeva@outlook.com>
2025-05-27 22:32:45 +0100

    Update comment

--- docs/.vitepress/components/ProjectData.vue
+++ docs/.vitepress/components/ProjectData.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import ExternalLink from './ExternalLink.vue'
-import data from '../data/project.json'
+import { projectData } from '../composables/projectData'
 
 </script>
 
 <template>
-  <div class="project-data">
-    <h2>Project Data</h2>
-    <ul>
-      <li v-for="(project, index) in data" :key="index">
-        <ExternalLink class="project-name" :href="project.link" target="_blank">{{ project.name }}</ExternalLink>
-        <p>{{ project.description }}</p>
-      </li>
-    </ul>
+  <h1 class="project-header">some backend projects</h1>
+  <div class="project-data" v-for="(project, index) in projectData" :key="index">
+    <div>
+      <ExternalLink class="project-name" :href="project.link" target="_blank">{{ project.name }}</ExternalLink>
+      <p class="desc">Description: {{ project.description }}</p>
+      <p class="dt">Domain: {{ project.domain }}</p>
+      <p class="dt">Tools: {{ project.Tools.join(', ') }}</p>
+      <br /> 
+      
+    </div>
   </div>
 </template>
 
 <style scoped>
 
-.project-name {
-  color: var(--color-text-light);
+/* .project-header {
+  margin-bottom: 2em;
+  text-decoration: underline wavy var(--color-accent-light);
 }
 
+.project-name {
+  color: var(--color-text-light);
+  font-size: 1.5em;
+}
+
+.dt {
+  color: var(--color-accent-light);
+} */
 </style>
